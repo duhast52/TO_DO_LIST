9b8600fdbda102cd57401583eb4094963b1efa82
Maxim <papaduhast1@gmail.com>
2020-09-19 14:08:39 +0300

    commit 2

--- script.js
+++ script.js
@@ -1,50 +1,38 @@
-let myNodelist = document.getElementsByTagName('LI');
-for (let i = 0; i < myNodelist.length; i++) {
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
-
 let close = document.getElementsByClassName("close");
-for (let i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        let div = this.parentElement;
-        div.style.display = "none";
-    }
-}
-
 let list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
+let addBtn = document.querySelector('.addBtn');
+
+
+list.addEventListener('click', ev => {
     if (ev.target.tagName === 'LI') {
         ev.target.classList.toggle('checked');
     }
-}, false);
+});
 
-function newElement() {
+
+addBtn.addEventListener('click', () => {
     let li = document.createElement("li");
-    let inputValue = document.getElementById("myInput").value;
-    let t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '' || inputValue === ' ') {
-        alert("Поле не может быть пустым!");
+    let inputValue = document.querySelector("#myInput").value;
+    let inputText = document.createTextNode(inputValue);
+    li.append(inputText);
+    if (typeof inputValue == 'undefined' || !inputValue || inputValue.length === 0 || inputValue === "" || !/[^\s]/.test(inputValue) || /^\s*$/.test(inputValue) || inputValue.replace(/\s/g, "") === "") {
+        document.querySelector("#myInput").placeholder = "Поле не может быть пустым";
     } else {
-        document.getElementById("myUL").appendChild(li);
+        document.querySelector("#myUL").append(li);
     }
 
-    document.getElementById("myInput").value = "";
+    document.querySelector("#myInput").value = "";
 
     let span = document.createElement("SPAN");
     let txt = document.createTextNode("\u00D7");
     span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+    span.append(txt);
+    li.append(span);
 
     for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
+        close[i].addEventListener('click', function() {
             let div = this.parentElement;
             div.style.display = "none";
-        }
+        });
     }
-}
+});
